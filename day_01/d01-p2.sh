6ef2db826f085553186284fb8f094658eb96be4c
kwj <kwj@vlax.net>
2023-12-01 13:44:55 -0800

    Day 1

---
+++ day_01/d01-p2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Day 1: Part two
+
+if [ $# = 0 ]; then
+    echo "Usage: $0 <input file>"
+else
+    python3 preprocess.py $1 |
+    sed -E -e 's/^[^0-9]+//' -e 's/[^0-9]+$//' |
+    sed -E -e 's/^(.)$/\1\1/' -e 's/^(.).*(.)$/\1\2/' |
+    paste -s -d+ |
+    bc
+fi
