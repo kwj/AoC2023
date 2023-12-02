6ea8e380458195fb677a0fc447c2186ed9936f62
kwj <kwj@vlax.net>
2023-12-02 10:21:06 -0800

    Add comments

--- day_02/d02_p1.clj
+++ day_02/d02_p1.clj
@@ -5,21 +5,24 @@
 (require '[clojure.java.io :as io])
 (require '[clojure.string :as str])
 
+;;; user=> (make-game-info "Game 10: 2 red, 9 green, 8 blue; 16 green, 1 red, 7 blue; 3 blue, 5 red, 9 green; 5 blue, 2 red, 11 green")
+;;; {:number 10, :cubes {:red 5, :green 16, :blue 8}}
 (defn- make-game-info
+  "Return a map of game information which has game number and minimum required number of cubes by each color."
   [line]
   (let [[header game-data] (str/split line #": ")
         game-number (parse-long (nth (str/split header #" ") 1))
-        max-cubes (->> (re-seq #"[^,;\s]+" game-data)
-                       (partition 2)
-                       (map (fn [[v k]] {(keyword k) (parse-long v)}))
-                       (apply merge-with (fn [n1 n2] (max n1 n2))))]
-    {:number game-number :max-cubes max-cubes}))
+        cubes (->> (re-seq #"[^,;\s]+" game-data)
+                   (partition 2)
+                   (map (fn [[v k]] {(keyword k) (parse-long v)}))
+                   (apply merge-with (fn [n1 n2] (max n1 n2))))]
+    {:number game-number :cubes cubes}))
 
 (defn- possible?
   [thr-map game]
-  (and (<= (get-in game [:max-cubes :red] 0) (get thr-map :red))
-       (<= (get-in game [:max-cubes :blue] 0) (get thr-map :blue))
-       (<= (get-in game [:max-cubes :green] 0) (get thr-map :green))))
+  (and (<= (get-in game [:cubes :red] 0) (get thr-map :red))
+       (<= (get-in game [:cubes :blue] 0) (get thr-map :blue))
+       (<= (get-in game [:cubes :green] 0) (get thr-map :green))))
 
 (when (seq *command-line-args*)
   (->> (map make-game-info (line-seq (io/reader (first *command-line-args*))))
