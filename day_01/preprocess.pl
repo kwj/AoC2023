a2ade0058b35a362354db48c39d722617732308a
kwj <kwj@vlax.net>
2023-12-04 12:43:25 -0800

    Replace tabs to spaces

--- day_01/preprocess.pl
+++ day_01/preprocess.pl
@@ -10,12 +10,12 @@ sub replace_number_letters {
     my @cands = sort {$a->[0] <=> $b->[0]} grep {$_->[0] != -1} map {[index($s, $n_letters[$_]), $n_letters[$_], $_ + 1]} 0..$#n_letters;
 
     if (!@cands) {
-	return $s;
+        return $s;
     } else {
-	my $offset = $cands[0][0];
-	my $num = $cands[0][2];
-	substr($s, $offset, 1, "$num");
-	replace_number_letters($s);
+        my $offset = $cands[0][0];
+        my $num = $cands[0][2];
+        substr($s, $offset, 1, "$num");
+        replace_number_letters($s);
     }
 }
 
