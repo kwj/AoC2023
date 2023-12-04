c83100fe385e0f6e6f1119a4dae9c537169c6a03
kwj <kwj@vlax.net>
2023-12-04 13:48:36 -0800

    Use substitution by regex with the `g` modifier

--- day_01/preprocess.pl
+++ day_01/preprocess.pl
@@ -1,24 +1,25 @@
 #!/usr/bin/env perl
 
 # It looks like that 'oneight' is equal to '1.*8.*' not '1ight'.
+# I therefore replace a numeric word as following.
+#
+#   'one' -> 'one1one'
+#   'oneight' -> 'one1oneight8eight'
 
+use v5.12;
 use strict;
 use warnings;
 
 sub replace_number_letters {
     my $s = shift(@_);
     my @n_letters = qw(one two three four five six seven eight nine);
-    
-    my @cands = sort {$a->[0] <=> $b->[0]} grep {$_->[0] != -1} map {[index($s, $n_letters[$_]), $n_letters[$_], $_ + 1]} 0..$#n_letters;
 
-    if (!@cands) {
-        return $s;
-    } else {
-        my $offset = $cands[0][0];
-        my $num = $cands[0][2];
-        substr($s, $offset, 1, "$num");
-        return replace_number_letters($s);
+    while (my ($idx, $word) = each @n_letters) {
+        $idx += 1;
+        $s =~ s/${word}/${word}${idx}${word}/g;
     }
+
+    return $s;
 }
 
 while (<>) {
