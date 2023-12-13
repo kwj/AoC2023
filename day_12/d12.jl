316aa5e8566f33819beefb9835a4ca2e5b7b3a98
kwj <kwj@vlax.net>
2023-12-13 08:30:30 -0800

    Add comment

--- day_12/d12.jl
+++ day_12/d12.jl
@@ -12,6 +12,7 @@ function d12_p1(fname::String)
     ans
 end
 
+
 function d12_p2(fname::String)
     data = map(split.(readlines(fname), " ")) do v
                [join(repeat([v[1]], 5), "?"), repeat(map(x -> parse(Int, x), split(v[2], ",")), 5)]
@@ -23,20 +24,29 @@ function d12_p2(fname::String)
     ans
 end
 
+
 #=
 Each ghost has own state.
-  1) a position in the row (pos)
-  2) number of broken spring group being checked (grp_idx)
-  3) number of consecutive broken springs in a group (consec_num)
-  4) permission of broken spring (not_broken)
-
-[example]
-
-  s = "..??#???##??#??"
-           ^
-       `pos` = 5
-
+  [pos]
+    * a position in the row
+       s = "..??#???##??#??"
+                ^
+             pos = 5
+  [grp_idx]
+    * index of broken spring group under checking
+  [consec_num]
+    * number of consecutive broken springs in a group under checking
+       If the specified number of consecutive broken springs are found,
+       increase `grp_idx` by one and set `not_broken`, the broken spring
+       prohibition flag, is 1 for the next position.
+  [not_broken]
+    * prohibition of broken spring
+       If this flag is set (1), the condition of spring at the position
+       must be `operational` or `unknown`.
+       This flag is set only immediately after found a specified consecutive
+       damaged springs.
 =#
+
 function solve(s::AbstractString, grp::Vector{Int64})
     s_len = length(s)
     grp_len = length(grp)
@@ -50,12 +60,15 @@ function solve(s::AbstractString, grp::Vector{Int64})
                 if grp_idx > grp_len
                     ans += v
                 end
-                continue
             elseif (s[pos] == '#' || s[pos] == '?') && grp_idx <= grp_len && not_broken == 0
                 if s[pos] == '?' && consec_num == 0
-                    next_key = [pos + 1, grp_idx, consec_num, 0]
+                    # Treat this `unknown` spring as `operational`,
+                    # so divide a ghost into two.
+                    next_key = [pos + 1, grp_idx, 0, 0]
                     next_st[next_key] = get(next_st, next_key, 0) + v
                 end
+
+                # The following is the process for damaged spring
                 consec_num += 1
                 if consec_num == grp[grp_idx]
                     next_key = [pos + 1, grp_idx + 1, 0, 1]
