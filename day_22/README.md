37cff24f94bbf7726a8892c84c1f222dc52a17b9
kwj <kwj@vlax.net>
2023-12-22 16:45:28 -0800

    Check relationship between bricks instead of simulation

--- day_22/README.md
+++ day_22/README.md
@@ -4,8 +4,6 @@
 
 * [Babashka](https://babashka.org/) (confirmed to work with Babashka v1.3.186)
 
-Note: This solution is ugly and very slow.
-
 ## How to use
 
 *Part one* and *Part two*
--- day_22/d22.clj
+++ day_22/d22.clj
@@ -4,61 +4,76 @@
 
 (require '[clojure.java.io :as io])
 (require '[clojure.string :as str])
+(require 'clojure.set)
 
 (defn- parse-data
   [data]
-  (loop [lines (str/split data #"\n")
+  (loop [lines (map-indexed vector (str/split data #"\n"))
          result []]
     (if (seq lines)
-      (let [v (mapv parse-long (str/split (first lines) #"\W+"))]
+      (let [[id line] (first lines)
+            v (mapv parse-long (str/split line #"\W+"))]
         (recur (next lines)
-               (conj result {:x1 (nth v 0), :y1 (nth v 1), :z1 (nth v 2) :x2 (nth v 3) :y2 (nth v 4) :z2 (nth v 5)})))
+               (conj result {:id (keyword (str id))
+                             :xy (for [x (range (nth v 0) (inc (nth v 3)))
+                                       y (range (nth v 1) (inc (nth v 4)))]
+                                   [x y])
+                             :z1 (nth v 2)
+                             :z2 (nth v 5)})))
       (sort-by :z1 result))))
 
 (defn- drop-brick
   [brick ^java.util.HashMap m]
-  (let [max-height (apply max (for [x (range (:x1 brick) (inc (:x2 brick)))
-                                    y (range (:y1 brick) (inc (:y2 brick)))]
-                                (.getOrDefault m [x y] 0)))
-        delta (max (- (:z1 brick) max-height 1) 0)]
-    (assoc brick :z1 (- (:z1 brick) delta) :z2 (- (:z2 brick) delta))))
+  (loop [low (:z1 brick)]
+    (let [floors (remove nil? (map #(.get m (conj % (dec low))) (:xy brick)))]
+      (if (and (> low 1) (zero? (count floors)))
+        (recur (dec low))
+        (do (doall (map #(.put m (conj % (- (:z2 brick) (- (:z1 brick) low))) (:id brick)) (:xy brick)))
+            [(:id brick) (set floors)])))))
 
 (defn- drop-all-bricks
-  [bricks ^java.util.HashMap floor]
-  (loop [bricks bricks
-         v []
-         n-falls 0]
-    (if (seq bricks)
-      (let [b (first bricks)
-            new-b (drop-brick b floor)]
-          (doseq [x (range (:x1 new-b) (inc (:x2 new-b)))
-                  y (range (:y1 new-b) (inc (:y2 new-b)))]
-            (.put floor [x y] (:z2 new-b)))
-          (cond
-            (= (:z1 b) (:z1 new-b)) (recur (next bricks) (conj v new-b) n-falls)
-            :else (recur (next bricks) (conj v new-b) (inc n-falls))))
-      [v n-falls])))
-
-(defn- jenga
-  [tower]
-  (let [floor (java.util.HashMap.)]
-    (loop [bricks tower
-           n-safe-bricks 0
-           total-falls 0]
+  [bricks]
+  (let [height-map (java.util.HashMap.)]
+    (loop [bricks bricks
+           floors {}
+           ceils {}]
       (if (seq bricks)
-        (let [brick (first bricks)
-              bs (rest bricks)
-              [_ n-falls] (drop-all-bricks bs (java.util.HashMap. floor))]
-          (doseq [x (range (:x1 brick) (inc (:x2 brick)))
-                  y (range (:y1 brick) (inc (:y2 brick)))]
-            (.put floor [x y] (:z2 brick)))
-          (cond
-            (zero? n-falls) (recur (next bricks) (inc n-safe-bricks) total-falls)
-            :else (recur (next bricks) n-safe-bricks (+ total-falls n-falls))))
-        [n-safe-bricks total-falls]))))
+        (let [[id floor-bricks] (drop-brick (first bricks) height-map)]
+          (recur (next bricks)
+                 (assoc floors id floor-bricks)
+                 (reduce (fn [m k] (assoc m k (conj (get m k #{}) id)))
+                         (assoc ceils id #{})
+                         floor-bricks)))
+        [floors ceils]))))
+
+(defn- p1
+  [floors ceils]
+  (loop [ks (keys ceils)
+         cnt 0]
+    (if (seq ks)
+      (if (some #(= (count (% floors)) 1) ((first ks) ceils))
+        (recur (next ks) cnt)
+        (recur (next ks) (inc cnt)))
+      cnt)))
+
+(defn- p2
+  [floors ceils]
+  (let [cnt (atom 0)]
+    (loop [ks (keys ceils)]
+      (if (seq ks)
+        (let [brick (first ks)]
+          (loop [q (apply conj (clojure.lang.PersistentQueue/EMPTY) (brick ceils))
+                 fallen-bricks #{brick}]
+            (if (empty? q)
+              (swap! cnt + (dec (count fallen-bricks)))
+              (let [target (peek q)]
+                (if (clojure.set/subset? (target floors) fallen-bricks)
+                  (recur (apply conj (pop q) (target ceils)) (conj fallen-bricks target))
+                  (recur (pop q) fallen-bricks)))))
+          (recur (next ks)))
+        @cnt))))
 
 (when (seq *command-line-args*)
-  (let [[tower _] (drop-all-bricks (parse-data (slurp (first *command-line-args*))) (java.util.HashMap.))
-        [ans1 ans2] (jenga tower)]
-    (println "Part one:" ans1)
-    (println "Part two:" ans2)))
+  (let [[floors ceils] (drop-all-bricks (parse-data (slurp (first *command-line-args*))))]
+    (println "Part one:" (p1 floors ceils))
+    (println "Part two:" (p2 floors ceils))))
