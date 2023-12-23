aecc28d7b70331d300b0799ef50cc6c925d30d9a
kwj <kwj@vlax.net>
2023-12-22 20:19:55 -0800

    Tweak for readability

--- day_17/d17.nim
+++ day_17/d17.nim
@@ -1,9 +1,6 @@
 # Day 17
 
-import std/heapqueue
-import std/sequtils
-import std/strutils
-import std/tables
+import std/[heapqueue, sequtils, strutils, tables]
 
 type
   Grid = seq[seq[uint]]
@@ -11,21 +8,16 @@ type
     id: tuple[x: uint, y: uint, dir: uint]
     cost: uint
 
-
 proc `<`(a, b: Crucible): bool = a.cost < b.cost
-
 
 proc makeGrid(data: string): Grid =
   return splitLines(data).mapIt(it.mapIt(parseUInt($it)))
 
-
 proc isInside(grid: Grid, c: Crucible): bool =
   return c.id.x <= uint(high(grid)) and c.id.y <= uint(high(grid[c.id.x]))
 
-
 proc isGoal(grid: Grid, c: Crucible): bool =
   return c.id.x == uint(high(grid)) and c.id.y == uint(high(grid[high(grid)]))
-
 
 proc solve(grid: Grid, minSteps: uint, maxSteps: uint): uint =
   # high(uint) means `-1` in the `delta` array..
@@ -49,31 +41,30 @@ proc solve(grid: Grid, minSteps: uint, maxSteps: uint): uint =
     for (dx, dy) in delta[crucible.id.dir]:
       # I don't know why this is working. Deep copy or Copy-on-write?
       # I have not yet found documentation of this behavior.
-      var next_c = crucible
+      var nextCrucible = crucible
 
       # Change direction of movement
       # `0 xor 1` -> 1, `1 xor 1` -> 0
-      next_c.id.dir = next_c.id.dir xor 1
+      nextCrucible.id.dir = nextCrucible.id.dir xor 1
 
       for step in 1..max_steps:
-        next_c.id.x += dx
-        next_c.id.y += dy
-        if isInside(grid, next_c) == false:
+        nextCrucible.id.x += dx
+        nextCrucible.id.y += dy
+        if isInside(grid, nextCrucible) == false:
           break
-        next_c.cost += grid[next_c.id.x][next_c.id.y]
+        nextCrucible.cost += grid[nextCrucible.id.x][nextCrucible.id.y]
 
         if step < minSteps:
           continue
 
         # This `high(uint)` means that the total heat loss of reaching a block is unknown.
-        var crnt_cost = high(uint)
-        if costs.hasKey(next_c.id):
-          crnt_cost = costs[next_c.id]
+        var crntCost = high(uint)
+        if costs.hasKey(nextCrucible.id):
+          crntCost = costs[nextCrucible.id]
 
-        if next_c.cost < crnt_cost:
-          costs[next_c.id] = next_c.cost
-          hq.push(next_c)
-
+        if nextCrucible.cost < crntCost:
+          costs[nextCrucible.id] = nextCrucible.cost
+          hq.push(nextCrucible)
 
 when isMainModule:
   import std/cmdline
