20307b4fb54c29464848570593a67bf263bcc2ff
kwj <kwj@vlax.net>
2023-12-27 07:35:04 -0800

    Update comment

--- day_21/d21.jl
+++ day_21/d21.jl
@@ -37,11 +37,36 @@ function d21_p1(fname::String = "input")
     sum(W)
 end
 
+#=
+Part 2:
+
+This problem is a calculation of an area of some kind.
+
+The map data given in this problem, like the sample data, the edges are all empty.
+On the ohter hand, the row and column of the starting point `S` are also empty.
+
+And then, the size of the map is 131x131 and 26501365 = 131 * 202300 + 65. I therefore
+complement the answer with polynomial interpolation from the following three data points.
+
+  - 65 steps
+  - 65 + 131 steps
+  - 65 + 131 * 2 steps
+
+  Polynomial interpolation
+  https://en.wikipedia.org/wiki/Polynomial_interpolation
+
+Note:
+  The quadratic equation can be used in this problem because it is a calculation
+  of the area that spreads over with a certain cycle.
+=#
+
 function d21_p2(fname::String = "input")
     m = read_matrix(fname)
     starting_pos = findfirst(x -> x < 0, m)
     m[starting_pos] = 1
 
+    # Since the map is a square with an *odd* number of side length, there is no problem
+    # for the calculation of the (65 + 131*2) steps with the (131*3) * (131*3) map.
     M = repeat(m, 3, 3)
     W = zeros(Int, size(M))
     W[starting_pos] = 1
@@ -63,13 +88,12 @@ function d21_p2(fname::String = "input")
     # vandermode matrix for x=0:2
     V = [0, 1, 2] .^ transpose([0, 1, 2])
 
-    # cnt = Va
-    # --> a = inv(V) * cnt
+    # Va = cnt
+    #  --> a = inv(V) * cnt
     a = inv(V) * cnt
 
     # 26501365 steps = 65 + 131 * 202300 steps
-    # so, x = 202300.
-    # the answer is f(202300).
+    # so, the answer is f(202300) = a1 * 202300^0 + a2 * 202300^1 + a3 * 202300^2.
     Int(transpose(202300 .^ (0:2)) * a)
 end
 
